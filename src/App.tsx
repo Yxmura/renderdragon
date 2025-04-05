bc40d58e823348b4f1b1177654783b6a94ba2d11
Yamura <96625893+Yxmura@users.noreply.github.com>
2025-04-05 21:09:21 +0200

    ANALytics

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Analytics } from '@vercel/analytics/react';
+import { KBarProvider } from 'kbar';
 
 import Index from "./pages/Index";
 import ResourcesHub from "./pages/ResourcesHub";
@@ -20,7 +22,6 @@ import TOS from "./pages/TOS";
 import Privacy from "./pages/Privacy";
 import Construction from "./pages/Construction"
 import NotFound from "./pages/NotFound";
-import { KBarProvider } from 'kbar';
 
 const queryClient = new QueryClient();
 
@@ -48,6 +49,7 @@ const App = () => (
             <Route path="/construction" element={<Construction />} />
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
+            <Analytics />
           </Routes>
         </BrowserRouter>
       </TooltipProvider>
