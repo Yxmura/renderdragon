78cfe2ff31a77ca7f516501188bdb4cda082da19
Yamura <96625893+Yxmura@users.noreply.github.com>
2025-05-08 17:26:48 +0200

    some bugfixes regarding the navbar and utils page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -121,7 +121,9 @@ const Navbar = () => {
 
   return (
     <header 
-      className="top-0 left-0 right-0 z-50 transition-all duration-300"
+    className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
+      scrolled ? 'py-2' : 'py-4'
+    }`}
     >
       <div 
         className="absolute inset-0"
