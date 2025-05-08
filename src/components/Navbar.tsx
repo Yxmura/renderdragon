23d2aac5fcfd326467d6deb30e5e5652bb37525a
Yamura <96625893+Yxmura@users.noreply.github.com>
2025-05-08 18:33:05 +0200

    un-sticky navbar? might be cooking....

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -120,11 +120,7 @@ const Navbar = () => {
   };
 
   return (
-    <header 
-    className={`fixed top-0 left-0 right-0 z-50 transition-all duration-300 ${
-      scrolled ? 'py-2' : 'py-4'
-    }`}
-    >
+    <header className="left-0 right-0 z-50 transition-all duration-300 py-4">
       <div 
         className="absolute inset-0"
       />
